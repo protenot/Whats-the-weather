20d2a5fa936613c29ccc1fa6437b7fd5d95784a6
Olga_Belaya <protenot@yandex.ru>
2023-05-20 13:10:50 +0300

    established environment  till husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+  },
+};
