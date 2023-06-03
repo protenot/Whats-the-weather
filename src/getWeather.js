4f0a72f4ea5a4c83252fce3fb6f07dcfdd9c1420
Olga_Belaya <protenot@yandex.ru>
2023-06-03 16:21:17 +0300

    all code in JS no more HTML

--- src/getWeather.js
+++ src/getWeather.js
@@ -20,50 +20,86 @@ export async function getLocalWeather(inner) {
   inner.append(imgLocal);
 }
 export async function getWeather(wrapper) {
-  /*
-   * wrapper.innerHTML = <>
-   * <input placeholder="Введите название города"/>
-   * <button class = "main-button">Жми сюда </button>
-   * </>;
-   */
   const form = document.createElement("form");
   wrapper.prepend(form);
+  const h2 = document.createElement("h2");
+  h2.textContent = "Хочешь узнать погоду?";
+  wrapper.prepend(h2);
+
   const input = document.createElement("input");
-  input.placeholder = "Введите название города";
+  input.placeholder = "Введи название города";
   form.append(input);
+
   const button = document.createElement("button");
   button.innerText = "Жми сюда";
   form.append(button);
+
+  const city = document.createElement("div");
+  city.classList.add("city-name");
+  city.textContent = "Город : ";
+  wrapper.append(city);
+
+  const cityN = document.createElement("p");
+  cityN.classList.add("city-place");
+  city.append(cityN);
+
+  const temperatureData = document.createElement("p");
+  temperatureData.classList.add("city-name");
+  temperatureData.textContent = "Температура : ";
+  wrapper.append(temperatureData);
+
+  const avatar = document.createElement("img");
+  avatar.alt = "здесь будет картинка";
+  wrapper.append(avatar);
+
+  const h2WeatherBox = document.createElement("h2");
+  h2WeatherBox.textContent = "Вы недавно смотрели погоду в городах";
+  wrapper.append(h2WeatherBox);
+
+  const weatherBox = document.createElement("div");
+  weatherBox.id = "weather-box";
+  wrapper.append(weatherBox);
+
+  const h2Map = document.createElement("h2");
+  h2Map.textContent = "Это то, что Вы ищете?";
+  wrapper.append(h2Map);
+
+  const mapContainer = document.createElement("div");
+  mapContainer.id = "map-container";
+  wrapper.append(mapContainer);
 
   form.addEventListener("submit", async (ev) => {
     ev.preventDefault();
 
     let cityName = input.value;
-    input.value = "";
-    let weather = await hasWeather(cityName);
-    const cityN = document.querySelector(".city-name");
 
-    const temperatureData = document.querySelector(".temperature-data");
+    let weather = await hasWeather(cityName);
 
     let iconId = weather.weather[0].icon;
-    const avatar = document.querySelector(".avatar");
-    avatar.src = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
-    const imgBox = document.querySelector(".img-box");
-    cityN.textContent = weather.name;
-    temperatureData.textContent = `${Math.round(weather.main.temp)} °C`;
-    imgBox.append(avatar);
 
-    let weatherBox = document.querySelector("#weather-box");
+    avatar.src = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+    const cityN = document.createElement("p");
+
+    cityN.textContent = weather.name;
+    console.log(cityN.innerHTML);
+
+    city.textContent = `Город :  ${cityN.textContent}`;
+
+    temperatureData.textContent = `Температура :  ${Math.round(
+      weather.main.temp
+    )} °C`;
+
     const newButton = document.createElement("button");
     newButton.classList.add("list");
+    console.log(newButton);
+
     let { lon } = weather.coord;
-    console.log(lon);
+
     let { lat } = weather.coord;
-    console.log(lat);
-    const mapContainer = document.querySelector("#map-container");
+
     const mapFooter = document.createElement("img");
     mapFooter.classList.add("map");
-
     mapFooter.src = `https://static-maps.yandex.ru/1.x/?ll=${lon},${lat}&size=450,450&z=12&l=map`;
 
     let allMaps = document.querySelectorAll(".map");
@@ -74,35 +110,35 @@ export async function getWeather(wrapper) {
       mapContainer.append(mapFooter);
     } else {
       mapContainer.append(mapFooter);
-
-      // console.log(mapFooter)}
     }
 
     if (cityN.textContent) {
       newButton.textContent = cityN.textContent;
+      const newButtonText = newButton.textContent;
+      console.log(newButtonText);
+      const allButtons = weatherBox.querySelectorAll(".list");
+      const arrayNode = Array.from(allButtons, (a) => a.innerText);
 
-      console.log(weatherBox.innerText);
-
-      const cityButtons = Array.from(weatherBox.innerText).join("");
-      if (cityButtons.includes(newButton.textContent)) {
-        console.log(cityButtons);
+      if (arrayNode.includes(newButton.innerHTML)) {
+        console.log(arrayNode);
       } else {
         weatherBox.prepend(newButton);
       }
-      const allButtons = weatherBox.querySelectorAll(".list");
 
+      if (allButtons.length > 9) {
+        allButtons[9].remove();
+      }
       console.log(allButtons);
-      weatherBox = Array.from(cityButtons);
-
-      console.log(weatherBox);
 
       newButton.addEventListener("click", async () => {
         console.log(newButton.textContent);
         cityName = newButton.textContent;
         weather = await hasWeather(cityName);
-        cityN.textContent = weather.name;
+        city.textContent = `Город :  ${cityN.textContent}`;
         console.log(weather.main.temp);
-        temperatureData.textContent = `${Math.round(weather.main.temp)} °C`;
+        temperatureData.textContent = `Температура :  ${Math.round(
+          weather.main.temp
+        )} °C`;
         iconId = weather.weather[0].icon;
         avatar.src = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
         lon = weather.coord.lon;
@@ -118,14 +154,9 @@ export async function getWeather(wrapper) {
           mapContainer.append(mapFooter);
         } else {
           mapContainer.append(mapFooter);
-
-          // console.log(mapFooter)}
         }
       });
-
-      if (allButtons.length > 10) {
-        allButtons[10].remove();
-      }
     }
+    input.value = "";
   });
 }
