6a8a4f14b217163b33c336cb407299e4867eff5f
protenot <52546825+protenot@users.noreply.github.com>
2023-06-05 14:56:01 +0000

    Deploying to gh-pages from @ protenot/Whats-the-weather@350db7b2c73d3f2a559bf2db2744350554c65e6a 🚀

---
+++ 6d861f39e83d96cb5a1d.js
@@ -0,0 +1,1 @@
+import"./style.css";import{getLocalWeather,getWeather}from"./getWeather.js";getLocalWeather(document.querySelector("#header-container")),getWeather(document.querySelector("#container"));
