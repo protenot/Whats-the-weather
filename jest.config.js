56cbaee440fb54e202f147ea7fb0b8d0de966603
Olga_Belaya <protenot@yandex.ru>
2023-06-06 08:42:18 +0300

    try to make badge for coverage

--- jest.config.js
+++ jest.config.js
@@ -45,13 +45,7 @@ module.exports = {
 
   /*
    * A list of reporter names that Jest uses when writing coverage reports
-   * coverageReporters: [
-   *   "json",
-   *   "text",
-   *   "lcov",
-   *   "clover"
-   * ],
-   */
+   */ coverageReporters: ["json-summary"],
 
   /*
    * An object that configures minimum threshold enforcement for coverage results
